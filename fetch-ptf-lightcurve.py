d6c10a22acd56551e3d1dc202a9ed66f2c708594
Paul Chote <p.chote@warwick.ac.uk>
2017-02-23 10:13:16 +0000

    Cut points with > 0.5 magnitude correction.

--- fetch-ptf-lightcurve.py
+++ fetch-ptf-lightcurve.py
@@ -25,7 +25,7 @@ import urllib.request
 
 PTF_QUERY_URL = "http://irsa.ipac.caltech.edu/cgi-bin/Gator/nph-query?catalog=ptf_lightcurves"\
     "&spatial=cone&radius=2&radunits=arcsec&constraints=(fid%3E1)AND(goodflag%3E0)"\
-    "&selcols=hmjd,mag_autocorr,magerr_auto,fwhm_image,fwhmsex,ra,dec&outfmt=1&objstr={},{}"
+    "&selcols=hmjd,mag_autocorr,mag_auto,magerr_auto,fwhm_image,fwhmsex,ra,dec&outfmt=1&objstr={},{}"
 
 def print_usage(name):
     """Prints the utility help"""
@@ -40,20 +40,27 @@ def print_usage(name):
     return 1
 
 def get_filter(raw_data):
-    fwhm_ratio = raw_data[:, 3] / raw_data[:, 4]
+    fwhm_ratio = raw_data[:, 4] / raw_data[:, 5]
 
     # Start by excluding any points that are nan ('null' magnitude in the PTF database)
     # or negative ('null' zero point -> 0 in the PTF database)
     filt = raw_data[:, 1] > 0
 
     # We also want to discard anything that is more than twice the average field FWHM
-    filt = np.logical_and(filt, fwhm_ratio < 2)
+    filt = np.logical_and(filt, fwhm_ratio < 1.5)
+
+    # Finally any points that had more than 0.5 mag photometry correction
+    filt = np.logical_and(filt, abs(raw_data[:, 1] - raw_data[:, 2]) < 0.5)
 
     n = 0
     mean = None
     while True:
         if n > 50:
             print("Failed to converge on FWHM cut after 50 iterations")
+            break
+
+        # Filter discarded all the points!
+        if np.sum(filt) == 0:
             break
 
         last_mean = mean
@@ -84,24 +91,31 @@ if __name__ == '__main__':
         # Instead read line by line and manually filter comments before loading data
         lines = [line for line in data_request.readlines() if line[0] != 92 and line[0] != 124]
 
+        # Defaults for if there are no acceptable points
+        reference_hmjd = -2400000.5
+        outdata = np.empty(0)
+        excluded_points = 0
+        median_absolute_correction = 0
+
         # Write just the file header if there are no data
         if len(lines) > 0:
-            data = np.array(np.genfromtxt(lines, usecols=(0, 1, 2, 3, 4)), ndmin=2)
-
+            data = np.array(np.genfromtxt(lines, usecols=(0, 1, 2, 3, 4, 5)), ndmin=2)
             good_points = get_filter(data)
 
-            hmjd = data[good_points, 0]
-            reference_hmjd = np.min(hmjd)
-            outdata = np.transpose((hmjd - reference_hmjd, data[good_points, 1], data[good_points, 2]))
-            outdata = outdata[outdata[:, 0].argsort()]
-        else:
-            reference_hmjd = -2400000.5
-            outdata = np.empty(0)
+            if np.sum(good_points) > 0:
+                hmjd = data[good_points, 0]
+                reference_hmjd = np.min(hmjd)
+                outdata = np.transpose((hmjd - reference_hmjd, data[good_points, 1], data[good_points, 3]))
+                outdata = outdata[outdata[:, 0].argsort()]
+                excluded_points = len(data) - np.sum(good_points)
+                median_absolute_correction = np.median(abs(data[:,1] - data[:,2]))
 
         header = 'get-gator-lightcurve.py output file\n'
         header += 'RA: {}\n'.format(target_ra)
         header += 'Dec: {}\n'.format(target_dec)
         header += 'Reference HJD(UTC?): {}\n'.format(reference_hmjd + 2400000.5)
+        header += 'Excluded (bad mag / FWHM / correction): {}\n'.format(excluded_points)
+        header += 'Median absolute correction (mag): {:.4f}\n'.format(median_absolute_correction)
         header += 'HJD(UTC?) Mag  Error'
 
         np.savetxt(output, outdata, fmt='%.5f', header=header)
